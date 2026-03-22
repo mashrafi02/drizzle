dc2dd253ee387c4b59b63e0ec14cda8daf603e93
mashrafi <ahmedfirojmash365@gmail.com>
2026-03-22 20:27:52 +0600

    query style data select started and columns, extras, limit, offset fields done

--- src/main.ts
+++ src/main.ts
@@ -3,35 +3,24 @@ import { neon } from '@neondatabase/serverless';
 import { drizzle } from 'drizzle-orm/neon-http';
 import * as schema from './db/schema';
 import { UserTable } from './db/schema';
+import { sql } from 'drizzle-orm';
 
 
 const pgsql = neon(process.env.DATABASE_URL as string);
 const db = drizzle(pgsql, { schema, logger: true });
 
 async function main() {
-    const user = await db.insert(UserTable)
-                    .values([   // you can add multiple values with insert method
-                        {
-                            name: "Mashrafi",
-                            age: 25,
-                            email: 'mash@mail.com'
-                        },
-                        {
-                            name: "Deku",
-                            age: 24,
-                            email: "deku@hero.com"
-                        }
-                    ])
-                    .returning({    // return what you want and can even change the property name
-                        id: UserTable.id,
-                        emailAdd: UserTable.email,
-                    })
-                    .onConflictDoUpdate({   // do something when some thing conflicts
-                        target: UserTable.email,
-                        set: { name: "Updated name"}
-                    })
+    const users = await db.query.UserTable.findMany({
+        columns: { role: false }, // if you don'r specify any column, then it will return every field. but now it will return everything except for the role
+        extras: {
+            lowerCaseName: sql<string>`lower(${UserTable.name})`.as("lowerCaseName") // for writing raw sql queries
+        },
+        limit: 1, // how may results you want
+        offset: 1, // how many you want to skip from the start
+    })
 
-      console.log(user);
+    console.log(users);
+    
 }
 
 main()
