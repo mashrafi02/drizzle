eb01d3af044bad6a700b6c02b91ccc5795745f53
mashrafi <ahmedfirojmash365@gmail.com>
2026-03-22 22:43:07 +0600

    delete db done and drizzle crash course done

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,8 @@ import 'dotenv/config';
 import { neon } from '@neondatabase/serverless';
 import { drizzle } from 'drizzle-orm/neon-http';
 import * as schema from './db/schema';
-import { count, eq, gt } from 'drizzle-orm';
-import { UserTable } from './db/schema'
+import { eq } from 'drizzle-orm';
+import { UserTable, UserPreferencesTable } from './db/schema'
 
 
 const pgsql = neon(process.env.DATABASE_URL as string);
@@ -11,10 +11,11 @@ const db = drizzle(pgsql, { schema, logger: true });
 
 async function main() {
 
-    await db.update(UserTable)
-            .set({ age: 30 })
-            .where(eq(UserTable.age, 24))
-    // console.log(users);
+    await db.delete(UserPreferencesTable)
+        .where(eq(UserPreferencesTable.userId, "32d876bb-f50e-4faa-80c6-40604b85e897"));
+
+    await db.delete(UserTable)
+        .where(eq(UserTable.id, "32d876bb-f50e-4faa-80c6-40604b85e897"));
 }
 
 main()
