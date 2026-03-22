b4ef552224d8d5699b63ed7746bc26200a3d5ac0
mashrafi <ahmedfirojmash365@gmail.com>
2026-03-22 22:33:31 +0600

    update db done

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import 'dotenv/config';
 import { neon } from '@neondatabase/serverless';
 import { drizzle } from 'drizzle-orm/neon-http';
 import * as schema from './db/schema';
-import { count, gt } from 'drizzle-orm';
+import { count, eq, gt } from 'drizzle-orm';
 import { UserTable } from './db/schema'
 
 
@@ -11,16 +11,10 @@ const db = drizzle(pgsql, { schema, logger: true });
 
 async function main() {
 
-    // const users = await db.select({ id: UserTable.id, email: UserTable.email, emailUpdates: UserPreferencesTable.emailUpdates })
-    //                       .from(UserTable)
-    //                     //   .where(eq(UserTable.age, 24))
-    //                       .leftJoin(UserPreferencesTable, eq(UserPreferencesTable.userId, UserTable.id))
-
-    const users = await db.select({ name: UserTable.name, count: count(UserTable.name) })
-                          .from(UserTable)
-                          .groupBy(UserTable.name)
-                          .having(columns => gt(columns.count, 1))
-    console.log(users);
+    await db.update(UserTable)
+            .set({ age: 30 })
+            .where(eq(UserTable.age, 24))
+    // console.log(users);
 }
 
 main()
