1640329789e5732d0d9447b91180a088cbd88dc6
mashrafi <ahmedfirojmash365@gmail.com>
2026-03-22 12:25:55 +0600

    shema advanced done

---
+++ drizzle/0001_shiny_captain_stacy.sql
@@ -0,0 +1,34 @@
+CREATE TYPE "public"."userROle" AS ENUM('ADMIN', 'GUEST');--> statement-breakpoint
+CREATE TABLE "category" (
+	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
+	"name" varchar(255) NOT NULL
+);
+--> statement-breakpoint
+CREATE TABLE "postCategory" (
+	"postId" uuid NOT NULL,
+	"categoryId" uuid NOT NULL,
+	CONSTRAINT "postCategory_postId_categoryId_pk" PRIMARY KEY("postId","categoryId")
+);
+--> statement-breakpoint
+CREATE TABLE "post" (
+	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
+	"title" varchar(255) NOT NULL,
+	"averageRating" real DEFAULT 0 NOT NULL,
+	"createdAt" timestamp DEFAULT now() NOT NULL,
+	"updatedAt" timestamp DEFAULT now() NOT NULL,
+	"authorId" uuid NOT NULL
+);
+--> statement-breakpoint
+CREATE TABLE "userPreferences" (
+	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
+	"emailUpdates" boolean DEFAULT false NOT NULL,
+	"userId" uuid NOT NULL
+);
+--> statement-breakpoint
+ALTER TABLE "user" ADD COLUMN "email" varchar(255) NOT NULL;--> statement-breakpoint
+ALTER TABLE "user" ADD COLUMN "userRole" "userROle" DEFAULT 'GUEST' NOT NULL;--> statement-breakpoint
+ALTER TABLE "postCategory" ADD CONSTRAINT "postCategory_postId_post_id_fk" FOREIGN KEY ("postId") REFERENCES "public"."post"("id") ON DELETE no action ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE "postCategory" ADD CONSTRAINT "postCategory_categoryId_category_id_fk" FOREIGN KEY ("categoryId") REFERENCES "public"."category"("id") ON DELETE no action ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE "post" ADD CONSTRAINT "post_authorId_user_id_fk" FOREIGN KEY ("authorId") REFERENCES "public"."user"("id") ON DELETE no action ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE "userPreferences" ADD CONSTRAINT "userPreferences_userId_user_id_fk" FOREIGN KEY ("userId") REFERENCES "public"."user"("id") ON DELETE no action ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE "user" ADD CONSTRAINT "user_email_unique" UNIQUE("email");
--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,7 +1,63 @@
-import { pgTable, uuid, varchar } from "drizzle-orm/pg-core"
 
+import { boolean, index, integer, pgEnum, pgTable, primaryKey, real, timestamp, uuid, varchar } from "drizzle-orm/pg-core"
+
+
+export const UserRole = pgEnum("userROle", ["ADMIN", "GUEST"]);
 export const UserTable = pgTable("user", {
     id: uuid("id").primaryKey().defaultRandom(),
-    // id2: serial("id2").primaryKey(), for auto incremental integer values
-    name: varchar("name", { length: 255 }).notNull()
+    name: varchar("name", { length: 255 }).notNull(),
+    age: integer("age").notNull(),
+    email: varchar("email", { length:255 }).notNull().unique(),
+    role: UserRole("userRole").default("GUEST").notNull()
+}, table => [ // whatever you do in this extraConfig callback block, it will be accross the whole table.
+        index("emailIndex").on(table.email), // setting a index on the email field to query this table a little more faster
+        // emailIndex: uniqueIndex("emailIndex").on(table.email),    you could also do it like this. in this way you would'nt have to make email field unique in the actual table
+        // uniqueNameAndAge: unique("uniqueNameAndAge").on(table.name, table.age)     now in this table every name and age must be unique.
+])
+
+
+
+// one to one relationship --- one user can have one preference
+export const UserPreferencesTable = pgTable("userPreferences", {
+    id: uuid("id").primaryKey().defaultRandom(),
+    emailUpdates: boolean("emailUpdates").notNull().default(false),
+    userId: uuid("userId").references(() => UserTable.id, {onDelete: "no action"}).notNull()
+})
+
+
+// one to many relationship --- one user can have multiple posts
+export const PostTable = pgTable("post", {
+    id: uuid("id").primaryKey().defaultRandom(),
+    title: varchar("title", { length: 255 }).notNull(),
+    averageRating: real("averageRating").notNull().default(0),
+    createdAt: timestamp("createdAt").defaultNow().notNull(),
+    updatedAt: timestamp("updatedAt").defaultNow().notNull(),
+    authorId: uuid("authorId").references(() => UserTable.id ).notNull()
+})
+
+
+// many to many relationship --- one post can have multiple category and one categories can have multiple posts
+export const CategoryTable = pgTable("category", {
+    id: uuid("id").primaryKey().defaultRandom(),
+    name: varchar("name", { length: 255 }).notNull(),
+})
+
+
+export const PostCategoryTable = pgTable("postCategory", {
+    postId: uuid("postId").references(() => PostTable.id ).notNull(),
+    categoryId: uuid("categoryId").references(() => CategoryTable.id ).notNull(),
+}, table => [
+    primaryKey({ columns: [table.postId, table.categoryId]}) // you could also just add a id field with primeryKey method in the actucal table. But since the combo of postId and category id are always going to be unique, we can just use them to make a primery key for the table in the extraConfig callback
+])
+
+
+// you do some other interesting things like
+export const UserTestTable = pgTable("user", {
+    id: uuid("id").primaryKey().defaultRandom(),
+    name: varchar("name", { length: 255 }).notNull(),
+    // age: integer("age").notNull().array(), for array of numers instead of just a number
+    // age: integer("age").notNull().$type<12 | 24>(), for overwriting a fields default type 
+    // age: integer("age").notNull().$default(() => Math.random()), for running a function when the database runs each time 
+    email: varchar("email", { length:255 }).notNull().unique(),
+    role: UserRole("userRole").default("GUEST").notNull()
 })
