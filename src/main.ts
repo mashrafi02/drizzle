5c768a0442ead9bd02e8111779c359e6382747b9
mashrafi <ahmedfirojmash365@gmail.com>
2026-03-22 21:46:22 +0600

    other query methods scanned

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,6 @@ import 'dotenv/config';
 import { neon } from '@neondatabase/serverless';
 import { drizzle } from 'drizzle-orm/neon-http';
 import * as schema from './db/schema';
-import { UserTable, UserPreferencesTable } from './db/schema';
-import { sql } from 'drizzle-orm';
 
 
 const pgsql = neon(process.env.DATABASE_URL as string);
@@ -13,14 +11,9 @@ async function main() {
 
     const users = await db.query.UserTable.findMany({
         columns: { role: false }, // if you don'r specify any column, then it will return every field. but now it will return everything except for the role
-        with: {
-            preferences: {
-                columns: {
-                    emailUpdates : true
-                }
-            },
-            posts: true
-        }
+        // orderBy: desc(UserTable.age)     #or you can do this
+        // orderBy: (table, {asc}) => asc(table.age),
+        where: (table, funcs) => funcs.between(table.age, 20, 24)
     })
 
     console.log(users);
